30d9ef18be4867b7d7bc609363d8fa12115ca3d2
Loki <99037815+xxXLokiXxx@users.noreply.github.com>
2022-05-07 22:41:21 +0800

    commented

--- pokemon_main.cpp
+++ pokemon_main.cpp
@@ -20,10 +20,10 @@ pokemon_group Mypokemons;
 pokemon_group Rivalpokemons;
 
 int main() {
-  read_list();
+  read_list();                //function call list of gen one pokemon printed 
   listing_allpoke();
   string opt;
-  cout << "Enter \"Y\" to choose pokemons by yourself to battle. Enter \"N\" to randomly select pokemons to you." << endl;
+  cout << "Enter \"Y\" to choose pokemons by yourself to battle. Enter \"N\" to randomly select pokemons to you." << endl;  // user choose self selection or random generation of pokemons
   cout << "Your choice: ";
   while (opt != "Y" && opt != "N") {
     cin >> opt;
@@ -34,24 +34,24 @@ int main() {
     else
       cout << "Invalid input!" << endl;
   }
-  rand_pokemons(Rivalpokemons);
+  rand_pokemons(Rivalpokemons);       //function call to generate opponents pokemons 
   print_ours_pokemon(Mypokemons);
-  print_oppo_pokemon(Rivalpokemons);
+  print_oppo_pokemon(Rivalpokemons);       // print each sides pokemons
 
   ///battle_start///
   pokemon current_Mypokemons= Mypokemons.A;
   pokemon current_Rivalpokemons= Rivalpokemons.A;
-  print_match_start();
+  print_match_start();                                 //function call to print interface
   print_battle(current_Mypokemons,current_Rivalpokemons);
 
   string option;
   double Dmg_dealt;
   int mycounter = 0, rivalcounter = 0;
 
-  while ((mycounter != 6) && (rivalcounter != 6)) {
+  while ((mycounter != 6) && (rivalcounter != 6)) {           //while loop to continue battling till one side or two sides' pokemons all fainted
     receive_input(option);
 
-    if (option == "A") {
+    if (option == "A") {       // user chose A, to attact opponent
       print_battle_1stcolumn();
       dmg_deal_term_A_to_B(current_Rivalpokemons, current_Mypokemons, Dmg_dealt);
       cout << endl;
@@ -60,7 +60,7 @@ int main() {
       print_battle_2ndcolumn();
       print_battle(current_Mypokemons,current_Rivalpokemons);
     }
-    else if (option == "B") {
+    else if (option == "B") {    // user choose B, to heal
       print_battle_1stcolumn();
       heal(Mypokemons, current_Mypokemons, mycounter);
       cout << endl;
@@ -70,12 +70,12 @@ int main() {
       print_battle(current_Mypokemons,current_Rivalpokemons);
     }
     else if (option == "D") {
-      cout << "You escaped!" << endl;
+      cout << "You escaped!" << endl;     // user choose D to escape
       break;
     }
     else {
       cout << "Invalid input. Please only enter A/B/D!" << endl;
     }
   }
-  print_match_result(mycounter,rivalcounter);
+  print_match_result(mycounter,rivalcounter);     // function call to print who win and match results
 }
